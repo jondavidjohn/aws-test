56124b021af395877a787eff480f3c27cbb58317
Jonathan Johnson <me@jondavidjohn.com>
2023-05-24 13:30:04 -0500

    use ami method

--- main.tf
+++ main.tf
@@ -31,7 +31,24 @@ output "caller_identity_arn" {
   value = data.aws_caller_identity.current.arn
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
