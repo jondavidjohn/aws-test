e0d76f3ccc742e74a5c0ebe1eaad9459d7788e3a
Jonathan D. Johnson <me@jondavidjohn.com>
2024-01-31 13:21:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,3 +59,12 @@ resource "aws_instance" "web" {
     Name = "HelloWorld2"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld2"
+  }
+}
